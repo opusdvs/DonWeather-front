ca0edfea7efaf91afdb9e3434cdda5322760c010
Donatas <ktp211zdv@yandex.ru>
2026-02-12 22:08:45 +0300

    api

--- src/config/api.js
+++ src/config/api.js
@@ -6,8 +6,8 @@ export const API_ROUTES = {
 
 export const DEFAULT_CITY = 'Москва'
 
-//export const API_ENDPOINT = 'https://api.donweather.dev.buildbyte.ru'
-export const API_ENDPOINT = 'http://185.196.117.162:8082'
+export const API_ENDPOINT = 'https://api.donweather.dev.buildbyte.ru'
+//export const API_ENDPOINT = 'http://185.196.117.162:8082'
 
 // Интервал автоматического обновления погоды (в миллисекундах)
 export const WEATHER_UPDATE_INTERVAL_MS = 30 * 60 * 1000 // 1 час
